12a641b2c6bd59b96118adc4c9c20a8f0788ad78
Matt Noe <matthew.e.noe@gmail.com>
2015-06-26 21:14:54 -0400

    Added password check

--- public/javascripts/components/home/login/loginDirective.js
+++ public/javascripts/components/home/login/loginDirective.js
@@ -12,14 +12,22 @@ app.directive('loginDirective', [
       controller: function($scope, $state, authService) {
 
         $scope.showSignUp = false;
+
+        $scope.confirmPassword = '';
         $scope.user = {};
 
         $scope.toggleLoginSignUp = function() {
           $scope.user = {};
           $scope.showSignUp = !$scope.showSignUp;
+          $scope.error = null;
         }
 
         $scope.register = function(){
+          if ($scope.confirmPassword != $scope.user.Password) {
+            $scope.error = {};
+            $scope.error.message = 'Passwords must match';
+            return;
+          }
           authService.register($scope.user).error(function(error){
             $scope.error = error;
           }).then(function(){
